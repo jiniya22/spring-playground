60e555f6fba86a98cebb9c8601aecbf6c419df9b
jini <jini@jiniworld.me>
2024-09-26 16:58:25 +0900

    :lipstick: 정적 리소스 변동에 따른 reload 테스트를 위한 코드 추가

---
+++ spring-boot/src/main/java/me/jiniworld/springboot/ViewController.java
@@ -0,0 +1,18 @@
+package me.jiniworld.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/v")
+@Controller
+class ViewController {
+
+    @GetMapping(value = "/home")
+    public String home(@RequestParam(value = "message", required = false) String message, Model model){
+        model.addAttribute("message", message);
+        return "home";
+    }
+}
